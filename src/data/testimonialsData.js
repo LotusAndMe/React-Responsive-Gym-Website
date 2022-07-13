9330380788f91c2cd49cea5c3d719f22c6ea0160
LotusAndMe <60803449+LotusAndMe@users.noreply.github.com>
2022-07-13 17:28:33 +0300

    implementing webp at data source file

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,25 +1,35 @@
+/** @format */
+
 import image1 from "../assets/t-image1.png";
+import image1_webp from "../assets/t-image1.webp";
 import image2 from "../assets/t-image2.jpg";
+import image2_webp from "../assets/t-image2.webp";
 import image3 from "../assets/t-image3.jpg";
+import image3_webp from "../assets/t-image3.webp";
 
 export const testimonialsData = [
   {
     image: image1,
+    imageWebp: image1_webp,
     review:
       "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
+    name: "MATHEW HENDRICKSON",
+    status: "ENTREPRENEUR",
   },
   {
     image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
-    status: 'COACH'
+    imageWebp: image2_webp,
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ",
+    name: "JOHN KEVIN",
+    status: "COACH",
   },
   {
-    image : image3,
-    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
-    name: 'FRANKLIN',
-    status: "CUSTOMER"
-  }
+    image: image3,
+    imageWebp: image3_webp,
+    review:
+      " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem",
+    name: "FRANKLIN",
+    status: "CUSTOMER",
+  },
 ];
