2def66dc763600b2c6b6f100b965aecb215d923a
LotusAndMe <60803449+LotusAndMe@users.noreply.github.com>
2022-07-13 16:39:34 +0300

    implementing footer webp images

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Footer.css'
 import Github from '../../assets/github.png'
+import Github_webp from '../../assets/github.webp'
 import Instagram from '../../assets/instagram.png'
+import Instagram_webp from '../../assets/instagram.webp'
 import LinkedIn from '../../assets/linkedin.png'
+import LinkedIn_webp from '../../assets/linkedin.webp'
 import Logo from '../../assets/logo.png'
+import Logo_webp from '../../assets/logo.webp'
 
 
 const Footer = () => {
@@ -12,12 +16,24 @@ const Footer = () => {
             <hr />
             <div className="footer">
                 <div className="social-links">
-                    <img src={Github} alt="" />
-                    <img src={Instagram} alt="" />
-                    <img src={LinkedIn} alt="" />
+                    <picture>
+                        <source srcset={Github_webp} />
+                        <img src={Github} alt="" />
+                    </picture>
+                    <picture>
+                        <source srcset={Instagram_webp} />
+                        <img src={Instagram} alt="" />
+                    </picture>
+                    <picture>
+                        <source srcset={LinkedIn_webp} />
+                        <img src={LinkedIn} alt="" />
+                    </picture>
                 </div>
                 <div className="logo-f">
-                    <img src={Logo} alt="" />
+                    <picture>
+                        <source srcset={Logo_webp} />
+                        <img src={Logo} alt="" />
+                    </picture>
                 </div>
             </div>
             <div className='blur footer-blur1'></div>
